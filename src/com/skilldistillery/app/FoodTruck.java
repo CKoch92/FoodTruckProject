7871175adb5cdb4a859cc9ab9ab6e62c4613c679
Caleb Koch <caleb.koch@gmail.com>
2021-05-22 13:56:04 -0600

    in progress upload

--- src/com/skilldistillery/app/FoodTruck.java
+++ src/com/skilldistillery/app/FoodTruck.java
@@ -1,5 +1,67 @@
 package com.skilldistillery.app;
 
 public class FoodTruck {
+	
+	private int truckID;
+	private String truckName;
+	private String foodType;
+	private double rating;
+	
+	
+	public FoodTruck(int truckID, String truckName, String foodType, double rating) {
+		super();
+		this.truckID = truckID;
+		this.truckName = truckName;
+		this.foodType = foodType;
+		this.rating = rating;
+	}
+
+	
+	
+
+//	Method to print output for menu selection 1.
+//	Method to print output for menu selection 2.
+//	Method to print output for menu selection 3.
+
+	public int getTruckID() {
+		return truckID;
+	}
+
+
+	public void setTruckID(int truckID) {
+		this.truckID = truckID;
+	}
+
+
+	public String getTruckName() {
+		return truckName;
+	}
+
+
+	public void setTruckName(String truckName) {
+		this.truckName = truckName;
+	}
+
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+	}
+
+
+	public double getRating() {
+		return rating;
+	}
+
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+	
+	
 
 }
--- src/com/skilldistillery/app/FoodTruckApp.java
+++ src/com/skilldistillery/app/FoodTruckApp.java
@@ -1,10 +1,49 @@
 package com.skilldistillery.app;
 
+import java.util.Scanner;
+
 public class FoodTruckApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner kb = new Scanner(System.in);
+	
+		int numberOfTrucks;
+		numberOfTrucks = 1;
+		
+		while (numberOfTrucks <= 5) {
+		System.out.print("Input the name of a food truck: ");
+		String truckName = kb.next();
+		if (truckName.equalsIgnoreCase("quit")) {
+			break;
+		}
+		int truckID = numberOfTrucks;
+		System.out.println();
+		System.out.print("Input the type of food served by the aforementioned food truck: ");
+		String foodType = kb.next();
+		System.out.println();
+		System.out.print("Rate the food truck on a scale of 1 to 10: ");
+		double rating = kb.nextDouble();
+		
+			if (numberOfTrucks == 1) {
+				FoodTruck truck1 = new FoodTruck(truckID, truckName, foodType, rating);
+			}
+			else if (numberOfTrucks == 2) {
+				FoodTruck truck2 = new FoodTruck(truckID, truckName, foodType, rating);
+			}
+			else if (numberOfTrucks == 3) {
+				FoodTruck truck2 = new FoodTruck(truckID, truckName, foodType, rating);
+			}
+			else if (numberOfTrucks == 4) {
+				FoodTruck truck2 = new FoodTruck(truckID, truckName, foodType, rating);
+			}
+			else if (numberOfTrucks == 5) {
+				FoodTruck truck2 = new FoodTruck(truckID, truckName, foodType, rating);
+			}
+		numberOfTrucks ++;
+		}
+		
+		FoodTruckApp app = new FoodTruckApp();
+		
 	}
 
 }
